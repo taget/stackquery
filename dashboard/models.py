9e20ce93a6590e8132abb11672c25b48cb1ffe5d
Arx Cruz <acruz@redhat.com>
2015-02-17 16:43:23 +0100

    Adding description column to CustomReport

--- dashboard/models.py
+++ dashboard/models.py
@@ -36,7 +36,7 @@ class Team(Base, DictSerializable):
 class Release(Base, DictSerializable):
     '''Release database representation'''
     __tablename__ = 'release'
-    id = (Column(Integer, primary_key=True))
+    id = Column(Integer, primary_key=True)
     created = Column(DateTime, default=datetime.now)
     modified = Column(DateTime, default=datetime.now, onupdate=datetime.now)
     name = Column('name', String(200))
@@ -72,3 +72,4 @@ class CustomReport(Base, DictSerializable):
     require_authentication = Column(Boolean, default=False)
     user = Column('user', String(35))
     password = Column('password', String(35))
+    description = Column('description', Text)
