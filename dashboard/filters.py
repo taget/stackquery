3c1a702dcd7d8d48f871b77dfd0bb1227e8e478d
Arx Cruz <acruz@redhat.com>
2015-02-17 15:54:34 +0100

    Adding format_url filter for big ulrs

--- dashboard/filters.py
+++ dashboard/filters.py
@@ -9,6 +9,13 @@ def format_mark(value):
     return marks
 
 
+def format_url(value):
+    if len(value) > 50:
+        return value[0:15] + '...' + value[-15:]
+    return value
+
+
 def init_app(app):
     """Initialize a Flask application with custom filters."""
     app.jinja_env.filters['format_mark'] = format_mark
+    app.jinja_env.filters['format_url'] = format_url
