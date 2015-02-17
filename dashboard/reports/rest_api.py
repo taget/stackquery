9c88688a3fea62d7a430222859d6aae13f8954d7
Arx Cruz <acruz@redhat.com>
2015-02-17 11:13:06 +0100

    Initial report commit Create the reports module Create the custom_report to handle all the custom report separated from the rest of the code, so it will be easy to make the changes when modules are implemented separate rest api for reports

---
+++ dashboard/reports/rest_api.py
@@ -0,0 +1,32 @@
+from flask import Blueprint
+from flask import jsonify
+
+from database import db_session
+from forms import CustomReport
+
+import simplejson as json
+
+report_rest_api = Blueprint('report_rest_api', __name__)
+
+
+def date_handler(obj):
+    return obj.isoformat() if hasattr(obj, 'isoformat') else obj
+
+
+@report_rest_api.route('/api/report/')
+def get_reports():
+    releases = CustomReport.query.all()
+    return json.dumps(list(releases), default=date_handler)
+
+
+@report_rest_api.route('/api/report/<int:report_id>/delete')
+def delete_report(report_id):
+    report = CustomReport.query.get(report_id)
+    if report is None:
+        request = jsonify({'status': 'Not Found'})
+        request.status = 404
+        return request
+
+    db_session.delete(report)
+    db_session.commit()
+    return jsonify({'status': 'OK'})
