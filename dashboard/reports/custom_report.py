045a857e9748665c837d89987c09fab2e2f8ec56
Arx Cruz <acruz@redhat.com>
2015-02-17 16:44:56 +0100

    Adding custom_report blueprint
    I'm separating the blueprints by directory, so it will became easier
    when we start to use this code as 3rd party modules

--- dashboard/reports/custom_report.py
+++ dashboard/reports/custom_report.py
@@ -1,3 +1,12 @@
 from flask import Blueprint
+from flask import render_template
 
-custom_report = Blueprint('custom_report', __name__)
+from models import CustomReport
+
+custom_report = Blueprint('custom_report', __name__, template_folder='templates')
+
+
+@custom_report.route('/reports/')
+def custom_report_index():
+    reports = CustomReport.query.all()
+    return render_template('reports/index.html', reports=reports)
